05ffab7673dffbc1ad607ecbc538438482cdb6c5
Saba <saba.alikhani@ymail.com>
2021-09-21 23:57:59 +0430

    Defined the blink and rotate top animation in js file with setInterval

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const dot_one = document.querySelector("div > div:nth-child(1)");
+const loader = document.querySelector(".loader");
 
 setInterval(() => {
   let dot_one_blink_animation = dot_one.style.animation;
 
   if (!dot_one_blink_animation.includes("blink-animation")) {
-    dot_one.style.animation = "blink-animation 0.6s infinite 0.2s";
+    dot_one.style.animation = "blink-animation 0.6s infinite 0.1s";
   } else {
     dot_one.style.animation = "none";
   }
 }, 2000);
+
+setInterval(() => {
+  let loader_rotate_top = loader.style.animation;
+
+  if (!loader_rotate_top.includes("loader-rotate-up")) {
+    loader.style.animation = "loader-rotate-up 1.5s ease-in-out 0.1s forwards";
+  } else {
+    loader.style.animation = "none";
+  }
+}, 4000);
