ff44392a947eae6d2ddb5f7833f302ef88783e50
Saba <saba.alikhani@ymail.com>
2021-09-23 20:47:19 +0330

    Modified the loader-rotate-up

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ setInterval(() => {
   let dot_one_blink_animation = dot_one.style.animation;
 
   if (!dot_one_blink_animation.includes("blink-animation")) {
-    dot_one.style.animation = "blink-animation 0.6s infinite 0.1s";
+    dot_one.style.animation = "blink-animation 1s infinite 0s";
   } else {
     dot_one.style.animation = "none";
   }
-}, 2000);
+}, 3000);
 
 setInterval(() => {
   let loader_rotate_up = loader.style.animation;
 
   if (!loader_rotate_up.includes("loader-rotate-up")) {
-    loader.style.animation = "loader-rotate-up 1s ease-out 0.1s forwards";
-  } else {
-    loader.style.animation = "none";
-  }
-}, 3000);
-
-setInterval(() => {
-  let loader_rotate_down = loader.style.animation;
-
-  if (!loader_rotate_down.includes("loader-rotate-down")) {
-    loader.style.animation = "loader-rotate-down 1s ease-out 0s forwards";
+    loader.style.animation = "loader-rotate-up 3s ease-out 0s forwards";
   } else {
     loader.style.animation = "none";
   }
 }, 4000);
+
+// setInterval(() => {
+//   let loader_rotate_down = loader.style.animation;
+
+//   if (!loader_rotate_down.includes("loader-rotate-down")) {
+//     loader.style.animation = "loader-rotate-down 1s ease-out 0s forwards";
+//   } else {
+//     loader.style.animation = "none";
+//   }
+// }, 4000);
