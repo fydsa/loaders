3bf31f5468a5eb6bbbe4def8dea9ebe1ee1fb154
Saba <saba.alikhani@ymail.com>
2021-09-24 20:50:17 +0330

    Modified the animations again

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ setInterval(() => {
   let dot_one_blink_animation = dot_one.style.animation;
 
   if (!dot_one_blink_animation.includes("blink-animation")) {
-    dot_one.style.animation = "blink-animation 1s infinite 0s";
+    dot_one.style.animation = "blink-animation 0.5s infinite";
   } else {
     dot_one.style.animation = "none";
   }
-}, 3000);
+}, 1000);
 
 setInterval(() => {
   let loader_rotate_up = loader.style.animation;
 
   if (!loader_rotate_up.includes("loader-rotate-up")) {
-    loader.style.animation = "loader-rotate-up 3s ease-out 0s forwards";
+    loader.style.animation = "loader-rotate-up 3s ease-out 1s";
   } else {
     loader.style.animation = "none";
   }
 }, 4000);
 
-// setInterval(() => {
-//   let loader_rotate_down = loader.style.animation;
+setInterval(() => {
+  let loader_rotate_down = loader.style.animation;
 
-//   if (!loader_rotate_down.includes("loader-rotate-down")) {
-//     loader.style.animation = "loader-rotate-down 1s ease-out 0s forwards";
-//   } else {
-//     loader.style.animation = "none";
-//   }
-// }, 4000);
+  if (!loader_rotate_down.includes("loader-rotate-down")) {
+    loader.style.animation = "loader-rotate-down 3s ease-out 1s";
+  } else {
+    loader.style.animation = "none";
+  }
+}, 8000);
