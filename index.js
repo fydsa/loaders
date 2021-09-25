dbc0938333fb93d44bbab5dac2db81da07c662a5
Saba <saba.alikhani@ymail.com>
2021-09-25 23:53:05 +0330

    Added the blink animation to the fifth dot

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const dot_one = document.querySelector("div > div:nth-child(1)");
+const dot_five = document.querySelector("div > div:nth-child(5)");
+
 const loader = document.querySelector(".loader");
 
 setInterval(() => {
   let dot_one_blink_animation = dot_one.style.animation;
+  let dot_five_blink_animation = dot_five.style.animation;
 
   if (!dot_one_blink_animation.includes("blink-animation")) {
     dot_one.style.animation = "blink-animation 0.5s infinite";
+    dot_five.style.animation = "blink-animation 0.5s infinite";
   } else {
     dot_one.style.animation = "none";
+    dot_five.style.animation = "none";
   }
 }, 1000);
 
